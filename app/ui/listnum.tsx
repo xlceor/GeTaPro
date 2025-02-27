4efb1bf3c5597bc5050760d8f82f8750c2ac68ae
Carlos Osorio <gimcode6@gmail.com>
2025-02-27 07:53:49 -0600

    .

--- app/ui/listnum.tsx
+++ app/ui/listnum.tsx
@@ -1,8 +1,13 @@
-import {inter} from '@/app/ui/fonts'
-export default function Listnum({num}) {
-    return (
-      <div className={`${inter.className} flex h-10 w-10 justify-center items-center bg-slate-600`}>
-          {num}
-      </div>
-    );
-  }
+import { inter } from '@/app/ui/fonts'
+
+interface ListnumProps {
+  num: number; // o string, dependiendo de lo que esperas
+}
+
+export default function Listnum({ num }: ListnumProps) {
+  return (
+    <div className={`${inter.className} flex h-10 w-10 justify-center items-center bg-slate-600`}>
+      {num}
+    </div>
+  );
+}
