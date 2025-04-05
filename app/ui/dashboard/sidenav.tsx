00f15b698d8b9b9d12b094210dc290a2c187bbd5
Carlos Osorio <gimcode6@gmail.com>
2025-04-04 19:01:45 -0600

    .

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -3,7 +3,6 @@
 import Link from 'next/link';
 import NavLinks from '@/app/ui/dashboard/nav-links';
 import { PowerIcon } from '@heroicons/react/24/outline';
-import { useState } from 'react';
 
 export default function SideNav({ children }: { children: React.ReactNode }) {
   return (
