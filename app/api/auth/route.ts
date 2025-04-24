4adcd0c34de32de39b72507642d888cd125937c8
Carlos Osorio <gimcode6@gmail.com>
2025-04-24 10:32:21 -0600

    removed authOptions

--- app/api/auth/route.ts
+++ app/api/auth/route.ts
@@ -1,45 +1,7 @@
-import NextAuth from 'next-auth';
-import type { NextAuthOptions } from 'next-auth';
-import GoogleProvider from 'next-auth/providers/google';
-import type { Session, User } from 'next-auth';
-import type { JWT } from 'next-auth/jwt';
+// app/api/auth/route.ts
 
-export const authOptions: NextAuthOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!
-    })
-  ],
-  callbacks: {
-    async signIn({ user }: { user: User }) {
-      try {
-        const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/add-user`, {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify(user)
-        });
-        return res.ok;
-      } catch (error) {
-        console.error('Error during signIn callback:', error);
-        return false;
-      }
-    },
-    async session({
-      session,
-      token
-    }: {
-      session: Session;
-      token: JWT;
-    }): Promise<Session> {
-      if (session.user && token.sub) {
-        // Aseguramos que el ID del usuario esté presente
-        (session.user as { id: string }).id = token.sub;
-      }
-      return session;
-    }
-  }
-};
+import NextAuth from 'next-auth';
+import { authOptions } from '@/app/lib/auth/authOptions';
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
