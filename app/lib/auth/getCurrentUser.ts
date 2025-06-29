32c31e244258219013928308b72df8c70b3b1314
Carlos Osorio <gimcode6@gmail.com>
2025-06-29 13:30:43 -0600

    remove old provider

--- app/lib/auth/getCurrentUser.ts
+++
@@ -1,8 +0,0 @@
-// /app/lib/auth/getCurrentUser.ts
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/lib/auth/authOptions";
-
-export async function getCurrentUser() {
-  const session = await getServerSession(authOptions);
-  return session?.user ?? null;
-}
