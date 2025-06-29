d5728dcd89f350c1b018270367df4107dcc36a9c
Carlos Osorio <gimcode6@gmail.com>
2025-06-29 13:33:01 -0600

    Delete middleware.ts

--- app/middleware.ts
+++
@@ -1,9 +0,0 @@
-// app/middleware.ts
-import NextAuth from 'next-auth';
-import { authConfig } from './api/auth/auth.config';
-
-export default NextAuth(authConfig).auth;
-
-export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'], // Excluir rutas no protegidas
-};
