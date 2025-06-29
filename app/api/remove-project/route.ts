ed9e13e20d7570cb7f72e993d2ccae573f3d9511
Carlos Osorio <gimcode6@gmail.com>
2025-06-29 13:25:11 -0600

    Update route.ts

--- app/api/remove-project/route.ts
+++ app/api/remove-project/route.ts
@@ -1,21 +1,17 @@
 import { removeProjectFromUser } from '@/app/lib/db';
-import { getCurrentUser } from '@/app/lib/auth/getCurrentUser';
 import { NextResponse } from 'next/server';
+
 
 export async function DELETE(req: Request) {
   try {
-    const user = await getCurrentUser();
-    if (!user?.id) {
-      return new Response(JSON.stringify({ error: 'Usuario no autenticado' }), { status: 401 });
-    }
 
-    const { projectId } = await req.json();
+    const { projectId, userId } = await req.json();
 
     if (!projectId) {
       return new Response(JSON.stringify({ error: 'ID del proyecto requerido' }), { status: 400 });
     }
 
-    const updatedProjects = await removeProjectFromUser(user.id, projectId);
+    const updatedProjects = await removeProjectFromUser(userId, projectId);
 
     return NextResponse.json({ status: 'ok', projects: updatedProjects });
   } catch (error) {
